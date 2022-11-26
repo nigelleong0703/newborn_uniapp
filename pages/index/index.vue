0282ef91f19096bf1daf9cdbd0c97cda493cffa4
rayheng030 <rokmeng@gmail.com>
2022-11-26 16:40:10 +0800

    commit first

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,93 +1,93 @@
 <template>
-  <view class="content">
-    <view class="header">
-      <image mode="aspectFit" src="/static/logo.jpg"></image>
+    <view class="content">
+        <view class="header">
+            <image mode="aspectFit" src="/static/logo.jpg"></image>
+        </view>
+        <view class="text-area">
+            <text class="title">{{ title }}</text>
+        </view>
+        <view class="btn-area">
+            <u-button class='button' type='default' size="large" color='orange' @click="navigateAdmin" text="管理员 Admin">
+            </u-button>
+            <u-button class='button' type='default' size='large' color='orange' @click="navigateNurse" text="护士 Nurse">
+            </u-button>
+            <u-button class='button' type='default' size='large' color='orange' disabled=true @click="navigateDoctor"
+                text="医生 Doctor">
+            </u-button>
+            <u-button class='button' type='default' size='large' color='orange' disabled=true @click="navigatePatient"
+                text="患者 Patient">
+            </u-button>
+        </view>
     </view>
-    <view class="text-area">
-      <text class="title">{{title}}</text>
     </view>
-    <view class="btn-area">
-      <u-button class='button' type='default' size="large" color='orange' @click="navigateAdmin" text="管理员 Admin">
-      </u-button>
-      <u-button class='button' type='default' size='large' color='orange' @click="navigateNurse" text="护士 Nurse">
-      </u-button>
-      <u-button class='button' type='default' size='large' color='orange' disabled=true @click="navigateDoctor"
-        text="医生 Doctor">
-      </u-button>
-      <u-button class='button' type='default' size='large' color='orange' disabled=true @click="navigatePatient"
-        text="患者 Patient">
-      </u-button>
-    </view>
-  </view>
-  </view>
 </template>
 
 
 <script>
-  export default {
+export default {
     data() {
-      return {
-        title: "欢迎来到NewBorn系统"
-      }
+        return {
+            title: "欢迎来到NewBorn系统"
+        }
     },
     onLoad() {
 
     },
     methods: {
-      navigateAdmin() {
-        uni.navigateTo({
-          url: '/pages/admin/admin-login'
-        })
-      },
-      navigateNurse() {
-        uni.navigateTo({
-          url: '/pages/nurse/nurse-login'
-        })
-      },
+        navigateAdmin() {
+            uni.navigateTo({
+                url: '/pages/admin/admin-login'
+            })
+        },
+        navigateNurse() {
+            uni.navigateTo({
+                url: '/pages/nurse/nurse-login'
+            })
+        },
 
     }
-  }
+}
 </script>
 
 <style>
-  .content {
+.content {
     height: 100vh;
     display: flex;
     flex-direction: column;
     align-content: center;
     justify-content: center;
-  }
+}
 
-  .header {
+.header {
     height: 30vh;
     display: flex;
     justify-content: center;
-  }
+}
 
-  .logo {
+.logo {
     margin-top: 200rpx;
     margin-left: auto;
     margin-right: auto;
     margin-bottom: 50rpx;
-  }
+}
 
-  .text-area {
+.text-area {
     display: flex;
     justify-content: center;
-  }
+}
 
-  .title {
+.title {
     font-size: 50rpx;
     font-weight: bold;
     color: #ffaa00;
-  }
+}
 
-  .btn-area {
+.btn-area {
     height: 70vh;
     display: flex;
     flex-direction: column;
     justify-content: space-evenly;
     padding: 15vw;
     font-weight: bold;
-  }
+}
 </style>
---
+++ pages/nurse/nurse-patient_info.vue
@@ -0,0 +1,68 @@
+<template>
+    <view class="content">
+        <view class="body">
+            <view class="text-area">
+                <text class="title">{{ title }}</text>
+            </view>
+            <view class="navigate-bar">
+                <u-tabbar :value="value1" @change="name => value1 = name" :fixed="true" :border="false"
+                    :placeholder="true" :safeAreaInsetBottom="true">
+                    <u-tabbar-item text="基本信息" icon="account" @click="click1"></u-tabbar-item>
+                    <u-tabbar-item text="输液记录" icon="pushpin-fill" @click="click1"></u-tabbar-item>
+                    <u-tabbar-item text="巡视记录" icon="eye-fill" @click="click1"></u-tabbar-item>
+                </u-tabbar>
+            </view>
+        </view>
+    </view>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: "患者: 王小杨",
+            value1: 0,
+        }
+    },
+
+    onLoad() {
+    },
+
+    methods: {
+        click1(e) {
+            console.log('click1', e);
+        }
+    }
+}
+</script>
+
+<style>
+.content {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+    justify-content: center;
+}
+
+.text-area {
+    display: flex;
+    justify-content: center;
+}
+
+.title {
+    font-size: 50rpx;
+    font-weight: bold;
+    color: #ffaa00;
+}
+
+.body {
+    height: 70vh;
+    display: flex;
+    flex-direction: column;
+}
+
+.navigate-bar {
+    height: 100vh;
+}
+</style>

9639085001c283349a5cb26f0c1c1d9e19b43846
rayheng030 <rokmeng@gmail.com>
2023-01-06 22:04:40 +0800

    nurse edit&delete function

--- pages/nurse/nurse-patient_check.vue
+++ pages/nurse/nurse-patient_check.vue
@@ -1,19 +1,19 @@
 <template>
-    <view class="content">
-        <view class="body">
-            <view class="text-area">
-                <text class="title">{{ title }}</text>
-            </view>
-            <view class="button">
-                <u-button type="primary" icon="plus" shape="circle" @click="add_ward"></u-button>
-            </view>
-            <view v-for="(check, index) in checkList" :key="check.seq">
-                <u-cell-group>
-                    <u-cell :title="'巡视记录' + '	' + (index + 1)" @click="cell_click(check)" :border='false'
-                        :isLink='true' customStyle="margin-bottom: 5px"></u-cell>
-                </u-cell-group>
-            </view>
-<!--            <view class="navigate-bar">
+  <view class="content">
+    <view class="body">
+      <view class="text-area">
+        <text class="title">{{ title }}</text>
+      </view>
+      <view class="button">
+        <u-button type="primary" icon="plus" shape="circle" @click="add_ward"></u-button>
+      </view>
+      <view v-for="(check, index) in checkList" :key="check.seq">
+        <u-cell-group>
+          <u-cell :title="'巡视记录' + '	' + (index + 1)" @click="cell_click(check)" :border='false' :isLink='true'
+            customStyle="margin-bottom: 5px"></u-cell>
+        </u-cell-group>
+      </view>
+      <!--            <view class="navigate-bar">
                 <u-tabbar :value="value5" @change="name => value5 = name" :fixed="true" :border="false"
                     :placeholder="true" :safeAreaInsetBottom="true">
                     <u-tabbar-item text="基本信息" icon="account" @click="patient_info"></u-tabbar-item>
@@ -21,137 +21,137 @@
                     <u-tabbar-item text="巡视记录" icon="eye-fill" @click="patient_check"></u-tabbar-item>
                 </u-tabbar>
             </view> -->
-			<tabBar-nurse :currentPage="2"></tabBar-nurse>
-        </view>
+      <tabBar-nurse :currentPage="2"></tabBar-nurse>
     </view>
+  </view>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            title: "巡视记录",
-            value5: 2,
-            checkList: [],
-            patient_id: '',
-        }
-    },
-
-    onLoad() {
-        let patient_info = uni.getStorageSync('selected_patient')
-        console.log(patient_info)
-        this.patient_id = patient_info.id
-        this.getCheck_list()
-    },
-
-    methods: {
-        add_ward() {
-            uni.navigateTo({
-                url: '/pages/patient/add-patient-ward?id=' + this.patient_id,
-                success(res) {
-                    console.log(res);
-                },
-                fail(err) {
-                    console.log(err);
-                }
-            })
-        },
-        patient_info() {
-            uni.navigateTo({
-                url: '/pages/nurse/nurse-patient_info',
-                success(res) {
-                    console.log(res);
-                },
-                fail(err) {
-                    console.log(err);
-                }
-            })
-        },
-        patient_transfusion() {
-            uni.navigateTo({
-                url: '/pages/nurse/nurse-patient_transfusion',
-                success(res) {
-                    console.log(res);
-                },
-                fail(err) {
-                    console.log(err);
-                }
-            })
-        },
-        patient_check() {
-            uni.navigateTo({
-                url: '/pages/nurse/nurse-patient_check',
-                success(res) {
-                    console.log(res);
-                },
-                fail(err) {
-                    console.log(err);
-                }
-            })
-        },
-        open(e) {
-            // console.log('open', e)
-        },
-        close(e) {
-            // console.log('close', e)
-        },
-        change(e) {
-            // console.log('change', e)
-        },
-        cell_click(check) {
-            uni.setStorageSync('selected_check', check)
-            console.log(uni.getStorageSync('selected_check'))
-            uni.navigateTo({
-                url: "nurse-patient_check-info"
-            })
-        },
-        getCheck_list() {
-            let path = '/api/check?patientId=' + this.patient_id
-            console.log(path)
-			//////////////////////////////////
-            this.$request.get(path).then(res => {
-                console.log(res)
-                this.checkList = res.data.check;
-            })
-        }
+  data() {
+    return {
+      title: "巡视记录",
+      value5: 2,
+      checkList: [],
+      patient_id: '',
     }
+  },
+
+  onLoad() {
+    let patient_info = uni.getStorageSync('selected_patient')
+    console.log(patient_info)
+    this.patient_id = patient_info.id
+    this.getCheck_list()
+  },
+
+  methods: {
+    add_ward() {
+      uni.navigateTo({
+        url: '/pages/patient/add-patient-ward?id=' + this.patient_id,
+        success(res) {
+          console.log(res);
+        },
+        fail(err) {
+          console.log(err);
+        }
+      })
+    },
+    patient_info() {
+      uni.navigateTo({
+        url: '/pages/nurse/nurse-patient_info',
+        success(res) {
+          console.log(res);
+        },
+        fail(err) {
+          console.log(err);
+        }
+      })
+    },
+    patient_transfusion() {
+      uni.navigateTo({
+        url: '/pages/nurse/nurse-patient_transfusion',
+        success(res) {
+          console.log(res);
+        },
+        fail(err) {
+          console.log(err);
+        }
+      })
+    },
+    patient_check() {
+      uni.navigateTo({
+        url: '/pages/nurse/nurse-patient_check',
+        success(res) {
+          console.log(res);
+        },
+        fail(err) {
+          console.log(err);
+        }
+      })
+    },
+    open(e) {
+      // console.log('open', e)
+    },
+    close(e) {
+      // console.log('close', e)
+    },
+    change(e) {
+      // console.log('change', e)
+    },
+    cell_click(check) {
+      uni.setStorageSync('selected_check', check)
+      console.log(uni.getStorageSync('selected_check'))
+      uni.navigateTo({
+        url: "nurse-patient_check-info"
+      })
+    },
+    getCheck_list() {
+      let path = '/api/check?patientId=' + this.patient_id
+      console.log(path)
+      //////////////////////////////////
+      this.$request.get(path).then(res => {
+        console.log(res)
+        this.checkList = res.data.check;
+      })
+    }
+  }
 }
 </script>
 
 <style>
 .content {
-    height: 75vh;
-    display: flex;
-    flex-direction: column;
-    align-content: center;
-    justify-content: center;
+  height: 75vh;
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  justify-content: center;
 }
 
 .text-area {
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 }
 
 .title {
-    font-size: 50rpx;
-    font-weight: bold;
-    color: #ffaa00;
+  font-size: 50rpx;
+  font-weight: bold;
+  color: #ffaa00;
 }
 
 .body {
-    height: 70vh;
-    display: flex;
-    flex-direction: column;
+  height: 70vh;
+  display: flex;
+  flex-direction: column;
 }
 
 .navigate-bar {
-    height: 100vh;
+  height: 100vh;
 }
 
 .button {
-    width: 15%;
-    margin-top: 2%;
-    margin-left: 83%;
-    padding-bottom: 20rpx;
+  width: 15%;
+  margin-top: 2%;
+  margin-left: 83%;
+  padding-bottom: 20rpx;
 }
 </style>
--- pages/nurse/nurse-patient_transfusion.vue
+++ pages/nurse/nurse-patient_transfusion.vue
@@ -1,19 +1,19 @@
 <template>
-    <view class="content">
-        <view class="body">
-            <view class="text-area">
-                <text class="title">{{ title }}</text>
-            </view>
-            <view class="button">
-                <u-button type="primary" icon="plus" shape="circle" @click="add_transfusion"></u-button>
-            </view>
-            <view v-for="(transfusion, index) in transfusionList" :key="transfusion.seq">
-                <u-cell-group>
-                    <u-cell :title="'输液记录' + '	' + (index + 1)" @click="cell_click(transfusion)" :border='false'
-                        :isLink='true' customStyle="margin-bottom: 5px"></u-cell>
-                </u-cell-group>
-            </view>
-<!--            <view class="navigate-bar">
+  <view class="content">
+    <view class="body">
+      <view class="text-area">
+        <text class="title">{{ title }}</text>
+      </view>
+      <view class="button">
+        <u-button type="primary" icon="plus" shape="circle" @click="add_transfusion"></u-button>
+      </view>
+      <view v-for="(transfusion, index) in transfusionList" :key="transfusion.seq">
+        <u-cell-group>
+          <u-cell :title="'输液记录' + '	' + (index + 1)" @click="cell_click(transfusion)" :border='false' :isLink='true'
+            customStyle="margin-bottom: 5px"></u-cell>
+        </u-cell-group>
+      </view>
+      <!--            <view class="navigate-bar">
                 <u-tabbar :value="value4" @change="name => value4 = name" :fixed="true" :border="false"
                     :placeholder="true" :safeAreaInsetBottom="true">
                     <u-tabbar-item text="基本信息" icon="account" @click="patient_info"></u-tabbar-item>
@@ -21,137 +21,137 @@
                     <u-tabbar-item text="巡视记录" icon="eye-fill" @click="patient_check"></u-tabbar-item>
                 </u-tabbar>
             </view> -->
-			<tabBar-nurse :currentPage="1"></tabBar-nurse>
-        </view>
+      <tabBar-nurse :currentPage="1"></tabBar-nurse>
     </view>
+  </view>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            title: '输液记录',
-            value4: 1,
-            transfusionList: [],
-            patient_id: '',
-        }
-    },
-
-    onLoad() {
-        let patient_info = uni.getStorageSync('selected_patient')
-        console.log(patient_info)
-        this.patient_id = patient_info.id
-        this.getTransfusion_list()
-    },
-
-    methods: {
-        add_transfusion() {
-            uni.navigateTo({
-                url: '/pages/patient/add-patient-transfusion?id=' + this.patient_id,
-                success(res) {
-                    console.log(res);
-                },
-                fail(err) {
-                    console.log(err);
-                }
-            })
-        },
-        patient_info() {
-            uni.navigateTo({
-                url: '/pages/nurse/nurse-patient_info',
-                success(res) {
-                    console.log(res);
-                },
-                fail(err) {
-                    console.log(err);
-                }
-            })
-        },
-        patient_transfusion() {
-            uni.navigateTo({
-                url: '/pages/nurse/nurse-patient_transfusion',
-                success(res) {
-                    console.log(res);
-                },
-                fail(err) {
-                    console.log(err);
-                }
-            })
-        },
-        patient_check() {
-            uni.navigateTo({
-                url: '/pages/nurse/nurse-patient_check',
-                success(res) {
-                    console.log(res);
-                },
-                fail(err) {
-                    console.log(err);
-                }
-            })
-        },
-        open(e) {
-            // console.log('open', e)
-        },
-        close(e) {
-            // console.log('close', e)
-        },
-        change(e) {
-            // console.log('change', e)
-        },
-		cell_click(transfusion) {
-			uni.setStorageSync('selected_transfusion', transfusion)
-			console.log(uni.getStorageSync('selected_transfusion'))
-		    uni.navigateTo({
-		        url: "nurse-patient_transfusion-info"
-		    })
-		},
-        getTransfusion_list() {
-            let path = '/api/transfusion?patientId=' + this.patient_id
-            console.log(path)
-			//////////////////////////////////
-            this.$request.get(path).then(res => {
-                console.log(res)
-                this.transfusionList = res.data.transfusion;
-            })
-        }
+  data() {
+    return {
+      title: '输液记录',
+      value4: 1,
+      transfusionList: [],
+      patient_id: '',
     }
+  },
+
+  onLoad() {
+    let patient_info = uni.getStorageSync('selected_patient')
+    console.log(patient_info)
+    this.patient_id = patient_info.id
+    this.getTransfusion_list()
+  },
+
+  methods: {
+    add_transfusion() {
+      uni.navigateTo({
+        url: '/pages/patient/add-patient-transfusion?id=' + this.patient_id,
+        success(res) {
+          console.log(res);
+        },
+        fail(err) {
+          console.log(err);
+        }
+      })
+    },
+    patient_info() {
+      uni.navigateTo({
+        url: '/pages/nurse/nurse-patient_info',
+        success(res) {
+          console.log(res);
+        },
+        fail(err) {
+          console.log(err);
+        }
+      })
+    },
+    patient_transfusion() {
+      uni.navigateTo({
+        url: '/pages/nurse/nurse-patient_transfusion',
+        success(res) {
+          console.log(res);
+        },
+        fail(err) {
+          console.log(err);
+        }
+      })
+    },
+    patient_check() {
+      uni.navigateTo({
+        url: '/pages/nurse/nurse-patient_check',
+        success(res) {
+          console.log(res);
+        },
+        fail(err) {
+          console.log(err);
+        }
+      })
+    },
+    open(e) {
+      // console.log('open', e)
+    },
+    close(e) {
+      // console.log('close', e)
+    },
+    change(e) {
+      // console.log('change', e)
+    },
+    cell_click(transfusion) {
+      uni.setStorageSync('selected_transfusion', transfusion)
+      console.log(uni.getStorageSync('selected_transfusion'))
+      uni.navigateTo({
+        url: "nurse-patient_transfusion-info"
+      })
+    },
+    getTransfusion_list() {
+      let path = '/api/transfusion?patientId=' + this.patient_id
+      console.log(path)
+      //////////////////////////////////
+      this.$request.get(path).then(res => {
+        console.log(res)
+        this.transfusionList = res.data.transfusion;
+      })
+    }
+  }
 }
 </script>
 
 <style>
 .content {
-    height: 75vh;
-    display: flex;
-    flex-direction: column;
-    align-content: center;
-    justify-content: center;
+  height: 75vh;
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  justify-content: center;
 }
 
 .text-area {
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 }
 
 .title {
-    font-size: 50rpx;
-    font-weight: bold;
-    color: #ffaa00;
+  font-size: 50rpx;
+  font-weight: bold;
+  color: #ffaa00;
 }
 
 .body {
-    height: 70vh;
-    display: flex;
-    flex-direction: column;
+  height: 70vh;
+  display: flex;
+  flex-direction: column;
 }
 
 .navigate-bar {
-    height: 100vh;
+  height: 100vh;
 }
 
 .button {
-    width: 15%;
-    margin-top: 2%;
-    margin-left: 83%;
-    padding-bottom: 20rpx;
+  width: 15%;
+  margin-top: 2%;
+  margin-left: 83%;
+  padding-bottom: 20rpx;
 }
 </style>
